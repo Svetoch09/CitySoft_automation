db47f55f76cdcf42354bee72a90c7a169c684f2e
Svetlana Paskevich <strays-binges.4d@icloud.com>
2025-12-06 14:30:11 +0100

    api_dtp_data -typo

--- data/api_dtp_data.py
+++ data/api_dtp_data.py
@@ -39,7 +39,7 @@ DTP_NEGATIVE_SCENARIOS = [
     ("areaFilterParams", "Kazan", "Неверные данные"),
     ("zonedDateFilterParams",
         {"dateFrom": "2019-12-31T23:00:00", "dateTo": "2020-12-31T22:59:59"},
-        "Неверный формат веремени",),
+        "Неверный формат веремени"),
     ("withDeadOrNot", "Yes", "Неверные данные"),
     ("withDeadOrNot", 123, "Неверный формат данных"),
 ]
